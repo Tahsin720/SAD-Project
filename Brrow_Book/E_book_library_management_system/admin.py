9136d16da7998faf3491560553d949caf48145d3
Tahsin720 <80281183+Tahsin720@users.noreply.github.com>
2022-01-07 17:12:17 +0600

    Delete Brrow_Book directory

--- Brrow_Book/E_book_library_management_system/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import my_request, book
-
-# Register your models here.
-admin.site.register(my_request)
-admin.site.register(book)
--- Brrow_Book/E_book_library_management_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EBookLibraryManagementSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'E_book_library_management_system'
